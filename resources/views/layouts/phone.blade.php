f2522e4cf4561df6a5278b75296333a0f19bb8bf
m45adiwinata <m45adiwinata@gmail.com>
2020-12-31 15:56:02 +0800

    layout update 1

--- resources/views/layouts/phone.blade.php
+++ resources/views/layouts/phone.blade.php
@@ -7,7 +7,7 @@
     <title>ESCAPER</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{asset('css/main.css')}}">
+    <link rel="stylesheet" href="{{asset('css/main.css?v=').time()}}">
     <style>
         @font-face {
             font-family: escaperfont;
